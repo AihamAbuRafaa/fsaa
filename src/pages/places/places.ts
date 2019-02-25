9aae986cb902c3bb7da6359f0948df148e8d9b3a
aihamaburafaa <aehm@runnableweb.com>
2019-02-25 20:26:32 +0200

    working on add places and view them

--- src/pages/places/places.ts
+++ src/pages/places/places.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, Modal } from 'ionic-angular';
 import { AddPlacePage } from '../add-place/add-place';
 import { Place } from '../../models/place';
 import { PlacesService } from '../../providers/places';
+import { PlacePage } from '../place/place';
 
 /**
  * Generated class for the PlacesPage page.
@@ -19,16 +20,22 @@ import { PlacesService } from '../../providers/places';
 export class PlacesPage {
   addPlacePage=AddPlacePage;
   places:Place[]=[];
-  constructor(public navCtrl: NavController, public navParams: NavParams,private placesSvc:PlacesService) {
+  constructor(public navCtrl: NavController,
+     public navParams: NavParams,
+     private placesSvc:PlacesService,
+     private modalCtrl : ModalController) {
 
   }
   ionViewWillEnter(){
-    this.places=this.placesSvc.loadPlaces();
-    
+    this.places=this.placesSvc.loadPlaces(); 
   }
-
   ionViewDidLoad() {
     console.log('ionViewDidLoad PlacesPage');
+  }
+  onOpenPlace(place:Place,index:number){
+    const modal=this.modalCtrl.create('PlacePage',{place:place,index:index});
+    modal.present();
+
   }
 
 }
